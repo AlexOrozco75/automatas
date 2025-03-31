82ed36c1b6bdefc2e14b0aa6f093a6f501843edf
AlexOrozco75 <21030825@itcelaya.edu.mx>
2025-03-31 14:20:56 -0600

    interfazG

--- interfazgrafica.py
+++ interfazgrafica.py
@@ -1,1 +1,14 @@
-print("hola mundo")
+import os
+os.environ['TCL_LIBRARY'] = r"C:\Users\panda\AppData\Local\Programs\Python\Python313\tcl\tcl8.6"
+
+
+import tkinter as tk
+
+def mostrar_mensaje():
+    ventana = tk.Tk()
+    ventana.title("Hola Mundo")
+    etiqueta = tk.Label(ventana, text="Hola, mundo", font=("Arial", 20))
+    etiqueta.pack(pady=20)
+    ventana.mainloop()
+
+mostrar_mensaje()
